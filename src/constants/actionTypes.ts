4a2d81f846dafa183df5bc644b77d734aa033234
syoichi kimita <kimi.syou.koma@gmail.com>
2020-09-10 09:02:03 +0900

    test commit for contribution

--- src/constants/actionTypes.ts
+++ src/constants/actionTypes.ts
@@ -2,3 +2,8 @@
 export const POST_IDEA_START = 'POST_IDEA_START';
 export const POST_IDEA_SUCCESS = 'POST_IDEA_SUCCESS';
 export const POST_IDEA_FAILURE = 'POST_IDEA_FAILURE';
+
+
+// test constant
+const test = 'TEST';
+const TEST = 'test';
