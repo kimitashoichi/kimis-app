ed8daee42e4a4739344f6bd90e5df6117a452bd8
syoichi kimita <kimi.syou.koma@gmail.com>
2020-10-12 20:53:09 +0900

    Add firebase hosting setting file

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -5,17 +5,17 @@ import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas'
 import rootReducer from './reducers';
 import { ExtendedWindow } from './typeDefinition/reduxDevToolExtensionType';
-import { compose } from '@material-ui/system';
+// import { compose } from '@material-ui/system';
 
-declare var window: ExtendedWindow;
+// declare var window: ExtendedWindow;
 
 export default function configureStore(history: History) {
   const sagaMiddleware = createSagaMiddleware();
-  const composeReduxDevToolExtension = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const enhanser = composeReduxDevToolExtension(applyMiddleware(sagaMiddleware));
+  // const composeReduxDevToolExtension = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  // const enhanser = composeReduxDevToolExtension(applyMiddleware(sagaMiddleware));
   const store = createStore(
     rootReducer(history),
-    enhanser
+    applyMiddleware(sagaMiddleware)
   )
 
   sagaMiddleware.run(rootSaga)
